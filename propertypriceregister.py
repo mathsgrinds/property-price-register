750dfd7267a1f64b50d4d67e07c5a8988ed913ca
Stephen <stephen@mathsgrinds.ie>
2016-10-26 23:53:36 +0100

    Update
    Update

--- propertypriceregister.py
+++ propertypriceregister.py
@@ -7,33 +7,31 @@ from bs4 import BeautifulSoup
 import unicodedata
 import re
 import urllib2
+import threading
 
-# Set this to "False" for faster downloading but of course without eircodes in the address
-witheircodes = True
-
-useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
-entry = ''
-browser = mechanize.Browser(factory=mechanize.RobustFactory())
 url = "https://www.propertypriceregister.ie/website/npsra/pprweb.nsf/PPR?OpenForm"
+useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
+browser = mechanize.Browser(factory=mechanize.RobustFactory())
 browser.addheaders = [('User-agent',useragent)]
 browser.open(url)
 browser.select_form(nr=0)
+
 AllCounties = browser.possible_items("County")
-AllYears = browser.possible_items("Year")
 AllCounties = [x for x in AllCounties if x]
+
+AllYears = browser.possible_items("Year")
 AllYears = [x for x in AllYears if x]
-AllYears.reverse()
-global filename
-filename = "propertypriceregister.csv"
-if not os.path.exists(filename):
-    if witheircodes:
-        header = '"Date","Price","Url","Address","Eircode","County"'+"\n"
-    else:
-        header = '"Date","Price","Url","Address","County"'+"\n"        
-    with open(filename, 'a') as f:
+
+
+if not os.path.exists("propertypriceregister.csv"):
+    header = '"Date","Price","Address","FullMarketPrice","RoutingKey","County","NewDwelling"'+"\n"      
+    with open("propertypriceregister.csv", 'a') as f:
         f.write(header)
 
-def eircode(address):
+
+def routingkey(address):
+    #Oddly An Post does not have addresses in Irish in their Database - these will always fail.
+    useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
     browser = mechanize.Browser(factory=mechanize.RobustFactory())
     url = "http://correctaddress.anpost.ie/pages/Search.aspx"
     browser.addheaders = [('User-agent', useragent)]
@@ -45,17 +43,26 @@ def eircode(address):
     request = browser.form.click()
     response = browser.submit()
     html = response.read()
-    soup = BeautifulSoup(html, "html.parser")
-    tag = soup.find(id="ctl00_body_hfTextToCopy")
-    try:
-        value = tag['value']
-        address = value.replace("\n",", ")
-        return str(address.split(",")[-1].strip(" "))
-    except:
-        return "NA" # Known Issue: Searching for addresses in Irish on An Post often fails -- odd
+    routingkey = "NA"
+    for x in re.findall("[A-Z][0-9][0-9]\s[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]", html):
+        routingkey = x.split(" ")[0]
+    return routingkey
 
-for year in AllYears:
+def New(url):
+    useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
+    browser.addheaders = [('User-agent',useragent)]
+    browser.open(url)
+    html = browser.response().read()
+    if "New Dwelling" in html:
+        return "TRUE"
+    else:
+        return "FALSE"
+    
+def worker(year):
     for county in AllCounties:
+        useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0"
+        url = "https://www.propertypriceregister.ie/website/npsra/pprweb.nsf/PPR?OpenForm"
+        browser.addheaders = [('User-agent',useragent)]
         browser.open(url)
         browser.select_form(nr=0)
         browser["Year"] = [year]
@@ -68,25 +75,33 @@ for year in AllYears:
         soup = BeautifulSoup(html, "html.parser")
         results = soup.find("table", {"class" : "resultsTable"})
         for row in results.findAll("tr"):
-            for col in row.findAll("td"):
-                try:
-                    contents = col.contents[0]
-                    smallsoup = BeautifulSoup(contents, "html.parser")
-                    entry += '"'+contents.replace("\n","")+'",'
-                except:
-                    contents = str(col.contents[0])
-                    smallsoup = BeautifulSoup(contents, "html.parser")
-                    link = smallsoup.find("a")
-                    entry += '"'+"https://www.propertypriceregister.ie/Website/npsra/PPR/npsra-ppr.nsf/"+link["href"].replace("\n","")+'",'
-                    if witheircodes:
-                        entry += '"'+link.contents[0].replace("\n","")+'","'+eircode(link.contents[0].replace("\n",""))+'","'+county+'"\n'
-                    else:
-                        entry += '"'+link.contents[0].replace("\n","")+'","'+county+'"\n'                        
-                    entry = str(unicodedata.normalize('NFKD', entry).encode('ascii','ignore'))
-            if witheircodes:
-                with open(filename,'a') as f: f.write(entry)
-                entry = ''
-        if not witheircodes:
-            with open(filename,'a') as f: f.write(entry)
-            entry = ''
-quit()
+            try:
+                r = str(row).split("</td>")
+                Date = re.sub('<[^>]*>', '', r[0])
+                Price = re.sub('<[^>]*>', '', r[1])
+                Price = Price[3:].replace(",","")
+                for x in re.findall("href=\".*\"\s", r[2]):
+                    NewDwelling = New("https://www.propertypriceregister.ie/website/npsra/PPR/npsra-ppr.nsf/"+x.replace('href="','').replace('"',''))
+                if " **" in Price:
+                    FullMarketPrice = "FALSE"
+                else:
+                    FullMarketPrice = "TRUE"
+                Price = Price.replace(" **","")
+                Address = re.sub('<[^>]*>', '', r[2]).upper()
+                RoutingKey = routingkey(Address)
+                entry =  '"'+Date+'","'+Price+'","'+Address+'","'+FullMarketPrice+'","'+county+'","'+RoutingKey+'","'+NewDwelling+'"'+"\n"
+                #print entry
+                with open("propertypriceregister.csv",'a') as f: f.write(entry)
+            except:
+                pass
+
+
+
+threads = []
+for year in AllYears:
+    time.sleep(20)
+    t = threading.Thread(target=worker, args=(year,))
+    threads.append(t)
+    t.start()
+    time.sleep(20)
+
